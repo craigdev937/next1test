79b93a5fdcbcbf3f25ca6aa71c7d830ad324641e
Craig Johnson <craigdev937@gmail.com>
2023-11-28 19:14:47 -0500

    first commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Next.js and RTK-Query
+
+Initial setup of the project.  Created with Next.js, TypeScript, React, and RTK-Query.
+
+
+
+
---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <React.Fragment>
+            <h1>NextJS ver 14</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis optio quia, voluptatem blanditiis non deleniti exercitationem nemo. Distinctio eum totam culpa ut sunt incidunt. Facere est temporibus possimus reprehenderit exercitationem!</p>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis optio quia, voluptatem blanditiis non deleniti exercitationem nemo. Distinctio eum totam culpa ut sunt incidunt. Facere est temporibus possimus reprehenderit exercitationem!</p>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis optio quia, voluptatem blanditiis non deleniti exercitationem nemo. Distinctio eum totam culpa ut sunt incidunt. Facere est temporibus possimus reprehenderit exercitationem!</p>
+        </React.Fragment>
+    );
+};
+
+
